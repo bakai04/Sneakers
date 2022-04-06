5f2901e0222588310902fb2c22670d1fed4e2342
bakai04 <joldoshovbakai04@gmail.com>
2022-04-06 22:52:54 +0600

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from "react";
 import Card from "./components/Cards"
 import Header from "./components/Header"
-import Drawer from "./components/Drawer"
+import Drawer from "./components/Drawer";
+
+const arr = [
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imgUrl: "/img/boots/1.jpg",},
+  {title: "Мужские Кроссовки Nike Air Max 270", price: 15670, imgUrl: "/img/boots/2.jpg",},
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 8499, imgUrl: "/img/boots/3.jpg",},
+  {title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imgUrl: "/img/boots/4.jpg",}
+];
 function App() {
   return(
 <div className="wrapper">
-    <Drawer />
-<Header />
-  <div className="content p-40">
-    <h1 className="mb-40">Все кроссовки</h1>
+  <Drawer />
+  <Header />
+<div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/Vector.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
     <div className="d-flex">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {arr.map((obj, index) => (
+        <Card 
+          title={obj.title}
+          price={obj.price}
+          imgUrl={obj.imgUrl}
+          key={index}
+          onClick={() => console.log(obj)}
+        />
+      ))}
     </div>
   </div>
 </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,21 @@
-function Cards(){
-    return (
-        <div className="card">
-          <div className="favorite">
-            <img src="/img/unliked.png" alt="Unliked" />
-          </div>
-          <img width={133} height={112} src="/img/boots/1.jpg" alt="Sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
+function Cards(props){
+  return (
+      <div className="card">
+        <div className="favorite">
+          <img src="/img/unliked.png" alt="Unliked" />
         </div>
-      );
+        <img width={133} height={112} src={props.imgUrl} alt="Sneakers" />
+        <h5>{props.title}</h5>
+        <div className="d-flex justify-between align-center">
+          <div className="d-flex flex-column">
+            <span>Цена:</span>
+            <b>{props.price}</b>
+          </div>
+          <button className="button" onClick={props.onClick}>
+            <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+          </button>
+        </div>
+      </div>
+    );
 }
 export default Cards;
